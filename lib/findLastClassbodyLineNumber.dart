90cd261fb54d630e53e4267adab7a9375252481b
LE VAN THANH <zero.lethanh@gmail.com>
2026-03-06 13:10:49 +0700

    no log

--- lib/findLastClassbodyLineNumber.dart
+++ lib/findLastClassbodyLineNumber.dart
@@ -38,8 +38,8 @@ int _findLastClassbodyLineNumber(String filePath) {
     final lineNumber = parseResult.lineInfo
         .getLocation(classNode.rightBracket.end)
         .lineNumber;
-    print('Found last class declaration ending at line: ');
-    print(lineNumber);
+    // print('Found last class declaration ending at line: ');
+    // print(lineNumber);
   }
 
   throw Exception('No class declarations found in $filePath');
