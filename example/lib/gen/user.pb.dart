7775f796d49e65a123fcee938c456f5e2f942a1c
LE VAN THANH <zero.lethanh@gmail.com>
2026-03-22 10:42:21 +0700

    feat: Add `User` protobuf definition, remove `fastfood_v2.proto`, and integrate `buf` for Dart code generation with updated dependencies.

---
+++ example/lib/gen/user.pb.dart
@@ -0,0 +1,124 @@
+// This is a generated file - do not edit.
+//
+// Generated from user.proto.
+
+// @dart = 3.3
+
+// ignore_for_file: annotate_overrides, camel_case_types, comment_references
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: curly_braces_in_flow_control_structures
+// ignore_for_file: deprecated_member_use_from_same_package, library_prefixes
+// ignore_for_file: non_constant_identifier_names, prefer_relative_imports
+
+import 'dart:core' as $core;
+
+import 'package:fixnum/fixnum.dart' as $fixnum;
+import 'package:protobuf/protobuf.dart' as $pb;
+
+export 'package:protobuf/protobuf.dart' show GeneratedMessageGenericExtensions;
+
+class User extends $pb.GeneratedMessage {
+  factory User({
+    $core.String? id,
+    $core.String? email,
+    $core.String? displayName,
+    $core.String? photoUrl,
+    $fixnum.Int64? lastLoginAt,
+  }) {
+    final result = create();
+    if (id != null) result.id = id;
+    if (email != null) result.email = email;
+    if (displayName != null) result.displayName = displayName;
+    if (photoUrl != null) result.photoUrl = photoUrl;
+    if (lastLoginAt != null) result.lastLoginAt = lastLoginAt;
+    return result;
+  }
+
+  User._();
+
+  factory User.fromBuffer($core.List<$core.int> data,
+          [$pb.ExtensionRegistry registry = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromBuffer(data, registry);
+  factory User.fromJson($core.String json,
+          [$pb.ExtensionRegistry registry = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromJson(json, registry);
+
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(
+      _omitMessageNames ? '' : 'User',
+      package: const $pb.PackageName(_omitMessageNames ? '' : 'user'),
+      createEmptyInstance: create)
+    ..aOS(1, _omitFieldNames ? '' : 'id')
+    ..aOS(2, _omitFieldNames ? '' : 'email')
+    ..aOS(3, _omitFieldNames ? '' : 'displayName')
+    ..aOS(4, _omitFieldNames ? '' : 'photoUrl')
+    ..aInt64(5, _omitFieldNames ? '' : 'lastLoginAt')
+    ..hasRequiredFields = false;
+
+  @$core.Deprecated('See https://github.com/google/protobuf.dart/issues/998.')
+  User clone() => deepCopy();
+  @$core.Deprecated('See https://github.com/google/protobuf.dart/issues/998.')
+  User copyWith(void Function(User) updates) =>
+      super.copyWith((message) => updates(message as User)) as User;
+
+  @$core.override
+  $pb.BuilderInfo get info_ => _i;
+
+  @$core.pragma('dart2js:noInline')
+  static User create() => User._();
+  @$core.override
+  User createEmptyInstance() => create();
+  @$core.pragma('dart2js:noInline')
+  static User getDefault() =>
+      _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<User>(create);
+  static User? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.String get id => $_getSZ(0);
+  @$pb.TagNumber(1)
+  set id($core.String value) => $_setString(0, value);
+  @$pb.TagNumber(1)
+  $core.bool hasId() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearId() => $_clearField(1);
+
+  @$pb.TagNumber(2)
+  $core.String get email => $_getSZ(1);
+  @$pb.TagNumber(2)
+  set email($core.String value) => $_setString(1, value);
+  @$pb.TagNumber(2)
+  $core.bool hasEmail() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearEmail() => $_clearField(2);
+
+  @$pb.TagNumber(3)
+  $core.String get displayName => $_getSZ(2);
+  @$pb.TagNumber(3)
+  set displayName($core.String value) => $_setString(2, value);
+  @$pb.TagNumber(3)
+  $core.bool hasDisplayName() => $_has(2);
+  @$pb.TagNumber(3)
+  void clearDisplayName() => $_clearField(3);
+
+  @$pb.TagNumber(4)
+  $core.String get photoUrl => $_getSZ(3);
+  @$pb.TagNumber(4)
+  set photoUrl($core.String value) => $_setString(3, value);
+  @$pb.TagNumber(4)
+  $core.bool hasPhotoUrl() => $_has(3);
+  @$pb.TagNumber(4)
+  void clearPhotoUrl() => $_clearField(4);
+
+  @$pb.TagNumber(5)
+  $fixnum.Int64 get lastLoginAt => $_getI64(4);
+  @$pb.TagNumber(5)
+  set lastLoginAt($fixnum.Int64 value) => $_setInt64(4, value);
+  @$pb.TagNumber(5)
+  $core.bool hasLastLoginAt() => $_has(4);
+  @$pb.TagNumber(5)
+  void clearLastLoginAt() => $_clearField(5);
+}
+
+const $core.bool _omitFieldNames =
+    $core.bool.fromEnvironment('protobuf.omit_field_names');
+const $core.bool _omitMessageNames =
+    $core.bool.fromEnvironment('protobuf.omit_message_names');
---
+++ example/lib/gen/user.pbjson.dart
@@ -0,0 +1,34 @@
+// This is a generated file - do not edit.
+//
+// Generated from user.proto.
+
+// @dart = 3.3
+
+// ignore_for_file: annotate_overrides, camel_case_types, comment_references
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: curly_braces_in_flow_control_structures
+// ignore_for_file: deprecated_member_use_from_same_package, library_prefixes
+// ignore_for_file: non_constant_identifier_names, prefer_relative_imports
+// ignore_for_file: unused_import
+
+import 'dart:convert' as $convert;
+import 'dart:core' as $core;
+import 'dart:typed_data' as $typed_data;
+
+@$core.Deprecated('Use userDescriptor instead')
+const User$json = {
+  '1': 'User',
+  '2': [
+    {'1': 'id', '3': 1, '4': 1, '5': 9, '10': 'id'},
+    {'1': 'email', '3': 2, '4': 1, '5': 9, '10': 'email'},
+    {'1': 'display_name', '3': 3, '4': 1, '5': 9, '10': 'displayName'},
+    {'1': 'photo_url', '3': 4, '4': 1, '5': 9, '10': 'photoUrl'},
+    {'1': 'last_login_at', '3': 5, '4': 1, '5': 3, '10': 'lastLoginAt'},
+  ],
+};
+
+/// Descriptor for `User`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List userDescriptor = $convert.base64Decode(
+    'CgRVc2VyEg4KAmlkGAEgASgJUgJpZBIUCgVlbWFpbBgCIAEoCVIFZW1haWwSIQoMZGlzcGxheV'
+    '9uYW1lGAMgASgJUgtkaXNwbGF5TmFtZRIbCglwaG90b191cmwYBCABKAlSCHBob3RvVXJsEiIK'
+    'DWxhc3RfbG9naW5fYXQYBSABKANSC2xhc3RMb2dpbkF0');
